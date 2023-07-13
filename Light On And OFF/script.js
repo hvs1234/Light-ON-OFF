f6a0652b247a201ed54b013575839c9f428b12bc
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-07-13 11:47:46 +0530

    Add files via upload

---
+++ Light On And OFF/script.js
@@ -0,0 +1,10 @@
+const btn1 = document.querySelector("#b1");
+const btn2 = document.querySelector("#b2");
+const img = document.getElementById('myimage');
+
+btn1.addEventListener('click',()=>{
+    img.src = 'bulb on.png'
+});
+btn2.addEventListener('click',()=>{
+    img.src = 'bulb off.png'
+});
