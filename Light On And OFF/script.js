b6652806c416abb2533a4e8c91dec2bdb00f72b7
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-07-13 11:48:50 +0530

    Update script.js

--- Light On And OFF/script.js
+++ Light On And OFF/script.js
@@ -8,3 +8,65 @@ btn1.addEventListener('click',()=>{
 btn2.addEventListener('click',()=>{
     img.src = 'bulb off.png'
 });
+
+// const btn1 = document.querySelector("#b1");
+// const btn2 = document.querySelector("#b2");
+// const img = document.getElementById('myimage');
+
+// btn1.addEventListener('click',()=>{
+//     img.src = 'bulb on.png'
+// });
+// btn2.addEventListener('click',()=>{
+//     img.src = 'bulb off.png'
+// });
+// const btn1 = document.querySelector("#b1");
+// const btn2 = document.querySelector("#b2");
+// const img = document.getElementById('myimage');
+
+// btn1.addEventListener('click',()=>{
+//     img.src = 'bulb on.png'
+// });
+// btn2.addEventListener('click',()=>{
+//     img.src = 'bulb off.png'
+// });
+// const btn1 = document.querySelector("#b1");
+// const btn2 = document.querySelector("#b2");
+// const img = document.getElementById('myimage');
+
+// btn1.addEventListener('click',()=>{
+//     img.src = 'bulb on.png'
+// });
+// btn2.addEventListener('click',()=>{
+//     img.src = 'bulb off.png'
+// });
+// const btn1 = document.querySelector("#b1");
+// const btn2 = document.querySelector("#b2");
+// const img = document.getElementById('myimage');
+
+// btn1.addEventListener('click',()=>{
+//     img.src = 'bulb on.png'
+// });
+// btn2.addEventListener('click',()=>{
+//     img.src = 'bulb off.png'
+// });
+// const btn1 = document.querySelector("#b1");
+// const btn2 = document.querySelector("#b2");
+// const img = document.getElementById('myimage');
+
+// btn1.addEventListener('click',()=>{
+//     img.src = 'bulb on.png'
+// });
+// btn2.addEventListener('click',()=>{
+//     img.src = 'bulb off.png'
+// });
+
+// const btn1 = document.querySelector("#b1");
+// const btn2 = document.querySelector("#b2");
+// const img = document.getElementById('myimage');
+
+// btn1.addEventListener('click',()=>{
+//     img.src = 'bulb on.png'
+// });
+// btn2.addEventListener('click',()=>{
+//     img.src = 'bulb off.png'
+// });
